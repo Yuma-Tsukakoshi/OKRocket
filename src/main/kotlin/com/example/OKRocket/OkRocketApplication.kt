c4488882ee33fb36eef2cf32c5a3ea94d9ee0e55
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-04-30 07:12:33 +0900

    init

---
+++ src/main/kotlin/com/example/OKRocket/OkRocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.OKRocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OkRocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<OkRocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/OKRocket/OkRocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.OKRocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OkRocketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
