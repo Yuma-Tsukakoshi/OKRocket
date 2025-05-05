1126c0d4d8d5fc346cb929a16e187c86d9a6fcae
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2025-05-06 00:26:11 +0900

    fix: fmt

--- src/main/kotlin/com/example/OKRocket/domain/team/TeamRepository.kt
+++ src/main/kotlin/com/example/OKRocket/domain/team/TeamRepository.kt
@@ -1,11 +1,7 @@
 package com.example.OKRocket.domain.team
 
-import com.example.OKRocket.domain.team.Team
-
-
 interface TeamRepository {
-  fun findById(id: Long): Team?
-  fun save(team: Team): Team
-  fun delete(team: Team)
+    fun findById(id: Long): Team?
+    fun save(team: Team): Team
+    fun delete(team: Team)
 }
-
--- src/main/kotlin/com/example/OKRocket/domain/user/UserRepository.kt
+++ src/main/kotlin/com/example/OKRocket/domain/user/UserRepository.kt
@@ -1,10 +1,7 @@
 package com.example.OKRocket.domain.user
 
-import com.example.OKRocket.domain.user.User
-
 interface UserRepository {
-  fun findById(id: Long): User?
-  fun save(user: User): User
-  fun delete(user: User)
+    fun findById(id: Long): User?
+    fun save(user: User): User
+    fun delete(user: User)
 }
-
